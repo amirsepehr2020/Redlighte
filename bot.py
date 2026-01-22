2b4b61e16da0e162de51ac5fb53a49368bf34e2f
amirsepehr2020 <sepehr2sodoury@gmail.com>
2026-01-22 18:57:25 +0330

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,36 @@
+import random
+
+respon = {
+    "سلام":{"score":2,"answer":["سلام به روی ماهت","سلام جیگر من"]},
+    "خوبی":{"score":1,"answer":["خوبم چطوری ","چطوری جیگر منم خوبم عزیزم"]}
+}
+
+normalize = {
+    "سلام": ["سلام", "درود", "سلاممم", "سلامم","سللم"],
+    "خوبی": ["خوبی", "چطوری", "حالت چطوره", "خوبی؟"]
+}
+
+
+def normalize_text(text):
+    for main_word, variants in normalize.items():
+        for v in variants:
+            if v in text:
+                text = text.replace(v, main_word)
+    return text
+
+
+
+def even(text):
+    call=[]
+    text= text.lower()
+    text=normalize_text(text)
+    for key,ssd in respon.items():
+        if key in text:
+            call.append((ssd["score"],random.choice(ssd["answer"])))
+    
+    if not call:
+        return "چه سخنی مهمی گفتی و من نفهمیدم؟"
+    
+    call.sort(reverse=True)
+    return " ".join([item[1] for item in call])
+
